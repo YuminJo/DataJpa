feaf38980fba24c513af17c305b1caa77103d8b7
조유민 <nationmilo@gmail.com>
2025-01-19 23:41:49 +0900

    feat: common interface

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -25,4 +25,24 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
     }
+    
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+        //리스트 조회 검증
+        Assertions.assertThat(memberRepository.findAll().size()).isEqualTo(2);
+        Assertions.assertThat(memberRepository.count()).isEqualTo(2);
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        Assertions.assertThat(memberRepository.count()).isEqualTo(0);
+    }
 }
